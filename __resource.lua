0970989b0d179968b071165d58256fc54b11237b
Sun <30409932+Origintk@users.noreply.github.com>
2019-02-01 19:58:12 -0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ]]
+ dependency "vrp"
 
 ui_page "nui/ui.html"
 
@@ -28,7 +29,8 @@ files {
 }
 
 client_script{
-			"@vrp/lib/utils.lua",
+			"lib/Proxy.lua",
+			"lib/Tunnel.lua",
 			"ui.lua",
 			"client.lua"
 		}
